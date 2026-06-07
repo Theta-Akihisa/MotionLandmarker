0b684e712c365c8b8d6b29c0fddd3d0f00cd0986
Theta-Akihisa <46920953+Theta-Akihisa@users.noreply.github.com>
2026-06-07 10:33:18 +0900

    Initial Commit

---
+++ SensorGlove_Monitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SensorGlove_Monitor
+//
+//  Created by Akihisa Shitara on 2026/06/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SensorGlove_Monitor/SensorGlove_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensorGlove_MonitorApp.swift
+//  SensorGlove_Monitor
+//
+//  Created by Akihisa Shitara on 2026/06/07.
+//
+
+import SwiftUI
+
+@main
+struct SensorGlove_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
